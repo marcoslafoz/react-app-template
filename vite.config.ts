3ffccc107c50ebbc9e3b10a832a99ff31d8e360d
marcoslafoz <marcos.lafoz96@gmail.com>
2025-04-25 01:42:30 +0200

    Se actualizaron las configuraciones de TailwindCSS

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,11 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { version } from './package.json'
 import path from 'path' 
+import tailwindcss from '@tailwindcss/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src')  // Añade esta configuración de alias
